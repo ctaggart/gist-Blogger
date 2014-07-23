8d5c989ab578ed825dd7ccefe78402a522fb6ba6
Cameron Taggart <cameron.taggart@gmail.com>
2014-07-23 17:36:51 -0500

    #1 loadGistBlogger after jQuery

--- gistLoader.js
+++ gistLoader.js
@@ -2,19 +2,8 @@
     
     var gistBloggerPath = "http://ctaggart.github.io/gist-Blogger/gistBlogger.js";
     var jquerySrc = "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js";
-
-    // Initialize the load the main script.
-    try {
-        initGist();
-    } catch(e) {
-        // load jquery if it is not already
-        if (typeof $ === 'undefined') {
-            var elem = document.createElement('script');
-            elem.src = jquerySrc;
-            var theBody = document.getElementsByTagName('body')[0];
-            theBody.appendChild(elem);
-        }
-        
+    
+    var loadGistBlogger = function() {
         var scriptId = 'gistPrinter';
         if (document.getElementById(scriptId) === null) {
             var elem = document.createElement('script');
@@ -25,6 +14,24 @@
             elem.src = gistBloggerPath;
             var theBody = document.getElementsByTagName('body')[0];
             theBody.appendChild(elem);
+        } 
+    }
+    
+    // Initialize the load the main script.
+    try {
+        initGist();
+    } catch(e) {
+        // load jquery if it is not already
+        if (typeof $ === 'undefined') {
+            var elem = document.createElement('script');
+            elem.src = jquerySrc;
+            elem.onload = loadGistBlogger;
+            var theBody = document.getElementsByTagName('body')[0];
+            theBody.appendChild(elem);
+        } else {
+            loadGistBlogger();
         }
+        
+
     }
 })();
