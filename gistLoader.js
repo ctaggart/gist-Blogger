e7cc39fa5b73cd4344bfdfbc7cf4e844b586d7df
Cameron Taggart <cameron.taggart@gmail.com>
2014-07-23 17:14:07 -0500

    #1 load query if it is not already

--- gistLoader.js
+++ gistLoader.js
@@ -1,14 +1,23 @@
 (function() {
     
     var gistBloggerPath = "http://ctaggart.github.io/gist-Blogger/gistBlogger.js";
+    var jquerySrc = "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js";
 
     // Initialize the load the main script.
     try {
         initGist();
     } catch(e) {
+        // load jquery if it is not already
+        if (! $) {
+            var elem = document.createElement('script');
+            elem.src = jquerySrc;
+            var theBody = document.getElementsByTagName('body')[0];
+            theBody.appendChild(elem);
+        }
+        
         var scriptId = 'gistPrinter';
         if (document.getElementById(scriptId) === null) {
-            var elem = document.createElement('SCRIPT');
+            var elem = document.createElement('script');
             elem.id = scriptId;
             elem.onload = function() {
               initGist();
